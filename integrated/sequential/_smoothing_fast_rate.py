a9e9b150eef4c8f3fe5d189f79686dc3cde3d3bf
Fatemeh-Yaghoobi <fatemeh.yaghoobi@aalto.fi>
2024-08-23 16:49:08 +0300

    fast-rate smoothing added

--- integrated/sequential/_smoothing_fast_rate.py
+++ integrated/sequential/_smoothing_fast_rate.py
@@ -5,46 +5,39 @@ from integrated._base import MVNStandard
 
 
 def smoothing(filter_trajectory: MVNStandard,
-              all_params):
-    last_interval = MVNStandard(filter_trajectory.mean[-1],
-                             filter_trajectory.cov[-1])
+              x_fast_rate_interval: MVNStandard,
+              fast_rate_params,
+              x_smoothed_slow_rate: MVNStandard):
+    xs = x_smoothed_slow_rate                                     # dim = (nx,),  nx x nx
+    flip_x_mean = jnp.flip(x_fast_rate_interval.mean, axis=0)     # dim = (l - 1) x nx,          index: k, l-1:1
+    flip_x_cov = jnp.flip(x_fast_rate_interval.cov, axis=0)       # dim = (l - 1) x nx x nx,     index: k, l-1:1
+    xf = MVNStandard(flip_x_mean, flip_x_cov)
 
-    other_intervals = MVNStandard(filter_trajectory.mean[:-1],
-                            filter_trajectory.cov[:-1])
-    def smooth_one(_all_params, xf, xs):
-        return _standard_smooth(_all_params, xf, xs)
-
-    def body(smoothed, inputs):
-        filtered = inputs
-        smoothed_state = smooth_one(all_params, filtered, smoothed)
-
-        return smoothed_state, smoothed_state
-
-    _, smoothed_states = jax.lax.scan(body,
-                                      last_interval,
-                                      other_intervals,
-                                      reverse=True)
+    smoothed_states_ = _standard_smooth(fast_rate_params, xf, xs)
 
     smoothed_states = jax.tree_map(lambda a, b: jnp.concatenate([b, a[None, ...]]),
-                                   last_interval, smoothed_states)
+                                   xs, smoothed_states_)
     return smoothed_states
 
 
-def _standard_smooth(all_params, Xf, Xs):
-    Mf, Pf = Xf
-    Ms, Ps = Xs
-    mf_l, Pf_l = Xf.mean[-1], Xf.cov[-1]
-    Abar, _, _, Bbar_u, Qbar, _, _, _, _, _, _ = all_params
+def _standard_smooth(fast_rate_params, Xf, xs):
+    Abar, Bbar, Gbar, Bbar_u, Qbar, C_bar, M_bar, D_bar, Rx, Q, Du_bar = fast_rate_params
 
-    mean_diff = Ms - (Bbar_u + Abar @ mf_l)    # dim = l x nx
-    S = Abar @ Pf_l @ jnp.transpose(Abar, axes=(0, 2, 1)) + Qbar
-    cov_diff = Ps - S
+    # note: the assumption is that all the inputs are the same.
+
+    Mf, Pf = Xf
+    ms, Ps = xs
 
     vmap_solve = jax.vmap(jax.scipy.linalg.solve, in_axes=(0, 0))
+    S = Abar @ Pf @ jnp.transpose(Abar, axes=(0, 2, 1)) + Qbar
     tempT = vmap_solve(S, Abar)
-    gain = Pf_l @ jnp.transpose(tempT, axes=(0, 2, 1))  # dim = l x nx x nx (should be)
-    Ms = Mf + jnp.einsum('ijk,ik->ij', gain, mean_diff) # dim = l x nx
-    Ps = Pf + gain @ cov_diff @ jnp.transpose(gain, axes=(0, 2, 1)) # dim = l x nx x nx
+    gain = Pf @ jnp.transpose(tempT, axes=(0, 2, 1))                 # dim = (l - 1) x nx x nx
 
-    return MVNStandard(Ms, Ps)
+    mean_diff = ms - (Bbar_u + Abar @ Mf)                            # dim = (l - 1) x nx
+    cov_diff = Ps - S                                                # dim = (l - 1) x nx x nx
+
+    Ms = Mf + jnp.einsum('ijk,ik->ij', gain, mean_diff)              # dim = (l - 1) x nx
+    Ps = Pf + gain @ cov_diff @ jnp.transpose(gain, axes=(0, 2, 1))  # dim = (l - 1) x nx x nx
+
+    return MVNStandard(jnp.flip(Ms, axis=0), jnp.flip(Ps, axis=0))
 
