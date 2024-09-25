0e8f187e21d19098828e5e536c4e976ca3068340
Fatemeh-Yaghoobi <fatemeh.yaghoobi@aalto.fi>
2024-09-25 14:48:03 +0300

    deleting extra files

--- tests/linear/csv_gpu_experiment_final_version.py
+++ tests/linear/csv_gpu_experiment_final_version.py
@@ -3,60 +3,58 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-with np.load("results/results_all_methods_final_version_L16_N4000/gpu_par_filter_times.npz") as loaded:
+with np.load("results/results_all_methods_final_version_L16_N6000/gpu_par_filter_times.npz") as loaded:
     gpu_par_filter_mean_times = loaded["gpu_par_filter_mean_times"]
     gpu_par_filter_median_times = loaded["gpu_par_filter_median_times"]
 
-with np.load("results/results_all_methods_final_version_L16_N4000/gpu_par_smooth_times.npz") as loaded:
+with np.load("results/results_all_methods_final_version_L16_N6000/gpu_par_smooth_times.npz") as loaded:
     gpu_par_smooth_mean_times = loaded["gpu_par_smooth_mean_times"]
     gpu_par_smooth_median_times = loaded["gpu_par_smooth_median_times"]
 
-with np.load("results/results_all_methods_final_version_L16_N4000/gpu_seq_filter_times.npz") as loaded:
+with np.load("results/results_all_methods_final_version_L16_N6000/gpu_seq_filter_times.npz") as loaded:
     gpu_seq_filter_mean_times = loaded["gpu_seq_filter_mean_times"]
     gpu_seq_filter_median_times = loaded["gpu_seq_filter_median_times"]
 
-with np.load("results/results_all_methods_final_version_L16_N4000/gpu_seq_smooth_times.npz") as loaded:
+with np.load("results/results_all_methods_final_version_L16_N6000/gpu_seq_smooth_times.npz") as loaded:
     gpu_seq_smooth_mean_times = loaded["gpu_seq_smooth_mean_times"]
     gpu_seq_smooth_median_times = loaded["gpu_seq_smooth_median_times"]
 
 n_length_space = 4
 l = 2**n_length_space
-N = 4000
-T = N * l
+N = 6000
+T = N
 lengths_space = np.logspace(n_length_space, int(np.log2(T)), num=10, base=2, dtype=int)
 
 
-plt.plot(lengths_space, gpu_par_filter_mean_times, label="par filter", linestyle="-.", linewidth=3)
-plt.plot(lengths_space, gpu_par_smooth_mean_times, label="par smoother", linestyle="-.", linewidth=3)
-plt.plot(lengths_space, gpu_seq_filter_mean_times, label="seq filter", linestyle="-.", linewidth=3)
-plt.plot(lengths_space, gpu_seq_smooth_mean_times, label="seq smoother", linestyle="-.", linewidth=3)
+plt.loglog(lengths_space, gpu_par_filter_mean_times, label="par filter", linestyle="-.", linewidth=3)
+plt.loglog(lengths_space, gpu_par_smooth_mean_times, label="par smoother", linestyle="-.", linewidth=3)
+plt.loglog(lengths_space, gpu_seq_filter_mean_times, label="seq filter", linestyle="-.", linewidth=3)
+plt.loglog(lengths_space, gpu_seq_smooth_mean_times, label="seq smoother", linestyle="-.", linewidth=3)
 plt.legend()
 plt.title("GPU Mean Run Times")
 plt.grid(True, which="both")
 plt.show()
 
 
-data = np.stack([lengths_space,
-                 gpu_par_filter_mean_times,
-                 gpu_par_filter_median_times,
-                 gpu_par_smooth_mean_times,
-                 gpu_par_smooth_median_times,
-                 gpu_seq_filter_mean_times,
-                 gpu_seq_filter_median_times,
-                 gpu_seq_smooth_mean_times,
-                 gpu_seq_smooth_median_times], axis=1)
-columns = ["lengths_space",
-           "gpu_par_filter_mean_times",
-           "gpu_par_filter_median_times",
-           "gpu_par_smooth_mean_times",
-           "gpu_par_smooth_median_times",
-           "gpu_seq_filter_mean_times",
-           "gpu_seq_filter_median_times",
-           "gpu_seq_smooth_mean_times",
-           "gpu_seq_smooth_median_times"]
-
-df = pd.DataFrame(data, columns=columns)
-df.to_csv("results/results_all_methods_final_version_L16_N4000/gpu-final-l16N4000.csv")
-
-
+# data = np.stack([lengths_space,
+#                  gpu_par_filter_mean_times,
+#                  gpu_par_filter_median_times,
+#                  gpu_par_smooth_mean_times,
+#                  gpu_par_smooth_median_times,
+#                  gpu_seq_filter_mean_times,
+#                  gpu_seq_filter_median_times,
+#                  gpu_seq_smooth_mean_times,
+#                  gpu_seq_smooth_median_times], axis=1)
+# columns = ["lengths_space",
+#            "gpu_par_filter_mean_times",
+#            "gpu_par_filter_median_times",
+#            "gpu_par_smooth_mean_times",
+#            "gpu_par_smooth_median_times",
+#            "gpu_seq_filter_mean_times",
+#            "gpu_seq_filter_median_times",
+#            "gpu_seq_smooth_mean_times",
+#            "gpu_seq_smooth_median_times"]
+#
+# df = pd.DataFrame(data, columns=columns)
+# df.to_csv("results/results_all_methods_final_version_L16_N6000/gpu-final-l16N6000.csv")
 
--- tests/linear/rmse_experiment.py
+++ tests/linear/rmse_experiment.py
@@ -1,35 +1,101 @@
 import jax
 import jax.numpy as jnp
+import numpy as np
+
+from integrated._utils import none_or_concat
 
 jax.config.update('jax_platform_name', 'cpu')
-
 jax.config.update("jax_enable_x64", True)
 
 from integrated._base import MVNStandard
+from integrated.inegrated_params import slow_rate_params, fast_rate_params, full_filtering_params
+from integrated.sequential import seq_filtering_slow_rate, seq_smoothing_slow_rate, filtering_fast_rate, filtering_all_states, smoothing_fast_rate
 from tests.linear.model import DistillationSSM
-
-from integrated.batch import *
-
 ################################### Parameters ########################################
-l = 4
-N = 20
+n_length_space = 4
+l = 2**n_length_space
+N = 200
 nx = 4
 ny = 2
 Q = 1
 R = 1
 prior_x = MVNStandard(jnp.zeros(4), jnp.eye(4))
-seed = 0
-################################ Model: Distillation Column ##########################
-model = DistillationSSM(l=l, interval=N, nx=nx, ny=ny, Q=Q, R=R, prior_x=prior_x, seed=seed)  # noqa
-x, h, y = model.get_data()
+n_runs = 100
+RMSE_filter = []
+RMSE_smoother = []
+for i in range(n_runs):
+    ################################ Model: Distillation Column ##########################
+    seed = np.random.randint(0, 1000)
+    model = DistillationSSM(l=l, interval=N, nx=nx, ny=ny, Q=Q, R=R, prior_x=prior_x, seed=seed)  # noqa
+    x, _, y = model.get_data()
+    ################################### Parameters ########################################
+    transition_model = model.TranParams()
+    observation_model = model.ObsParams()
+    Params_SR = slow_rate_params(transition_model, observation_model, l)
+    Params_FR = fast_rate_params(transition_model, observation_model, l)
+    Params_all_states = full_filtering_params(transition_model, model.ObsParams(), l)
 
-fast_sms, fast_sPs = batch_fast_smoother(model, y)
-print(f"{fast_sms.shape = }")
+    #################################### Filtering  ########################################
+    sequential_filtered = seq_filtering_slow_rate(y, prior_x, Params_SR)
+    ### Filtering - All States ###
+    m_l_k_1 = sequential_filtered.mean[:-1]
+    P_l_k_1 = sequential_filtered.cov[:-1]
+    vmap_func_all_states = jax.vmap(filtering_all_states, in_axes=(0, None, 0))
+    all_states_filtered_ = vmap_func_all_states(y, Params_all_states, MVNStandard(m_l_k_1, P_l_k_1))
+    all_filtering_means, all_filtering_covs = all_states_filtered_.mean, all_states_filtered_.cov
+    filtered_results = all_filtering_means.reshape(-1, 4)  # shape (N * l, nx)
+    ############################################### Smoothing - Slow rate - Sequential ########################################################
+    # Selection the filtering cross-covariances between the first and last states in each interval, i.e., P^f_{k, 1, l}.
+    def selected_Pf_k1l(Pf_all_, l):
+        def body(_, i):
+            return _, Pf_all_[i, 0:nx, (l-1)*nx:l*nx]
 
-fast_fms, fast_fPs = batch_fast_filter(model, y)
-print(f"{fast_fms.shape = }")
-################################### RMSE test ########################################
-rmse_filter = jnp.sqrt(jnp.mean((fast_fms[:, 0] - x[:, 0]) ** 2))
-rmse_smoother = jnp.sqrt(jnp.mean((fast_sms[:, 0] - x[:, 0]) ** 2))
-print(f"{rmse_filter = }")
-print(f"{rmse_smoother = }")
+        _, selected =  jax.lax.scan(body, init = None, xs=jnp.arange(N))
+        return selected
+    Pf_k1l = selected_Pf_k1l(all_filtering_covs, l)
+
+    # Selecting the first states of the filtering result in each interval, i.e., m^f_{1:N, 1} and P^f_{1:N, 1}.
+    def selected_fast_filtered(filtered_results, l):
+        def body(_, i):
+            return _, (MVNStandard(filtered_results.mean[i, 0:nx], filtered_results.cov[i, 0:nx, 0:nx]),
+                       MVNStandard(filtered_results.mean[i, (l-1)*nx:l*nx], filtered_results.cov[i, (l-1)*nx:l*nx, (l-1)*nx:l*nx]))
+
+        _, (selected_1, selected_l)  =  jax.lax.scan(body, init = None, xs=jnp.arange(0, N, 1))
+        return selected_1, selected_l
+    sr_filtered_k1, sr_filtered_kl = selected_fast_filtered(MVNStandard(all_filtering_means, all_filtering_covs), l)
+
+    # Slow-rate Smoothing
+    sequential_smoothed_slow_rate = seq_smoothing_slow_rate(sr_filtered_k1, sr_filtered_kl, transition_model, Pf_k1l)
+    ########## Smoothing - Fast rate
+    # Selection the filtering cross-covariances between each state and last states in each interval, i.e., P^f_{k, i, l} i = 1, ... , l.
+    def selected_Pf_k_all_l(Pf_all_, l):
+        def body(_, i):
+            return _, Pf_all_[i, :, (l-1)*nx:l*nx]
+
+        _, selected =  jax.lax.scan(body, init = None, xs=jnp.arange(N))
+        return selected
+    Pf_k_all_l = selected_Pf_k_all_l(all_filtering_covs, l)
+    # fast rate smoothing
+    vmap_function = jax.vmap(smoothing_fast_rate, in_axes=(0, 0, 0, None, 0))
+    sequential_smoothed_fast_rate = vmap_function(MVNStandard(all_filtering_means[:-1, :], all_filtering_covs[:-1, :, :]),
+                                                  MVNStandard(sr_filtered_kl.mean[:-1, :], sr_filtered_kl.cov[:-1, :, :]),
+                                                  MVNStandard(sequential_smoothed_slow_rate.mean[1:, :], sequential_smoothed_slow_rate.cov[1:, :, :]),
+                                                  transition_model,
+                                                  Pf_k_all_l[:-1, :, :])
+
+    sequential_smoothed_fast_rate = none_or_concat(sequential_smoothed_fast_rate,
+                                                   MVNStandard(all_filtering_means[-1, :], all_filtering_covs[-1, :, :]),
+                                                   0)
+    smoothed_results = sequential_smoothed_fast_rate.mean.reshape(-1, nx)  # shape (N * l, nx)
+    rmse_filter = jnp.sqrt(jnp.mean((filtered_results[:, 0] - x[1:, 0]) ** 2))
+    rmse_smoother = jnp.sqrt(jnp.mean((smoothed_results[:, 0] - x[1:, 0]) ** 2))
+    print(f"run {i + 1} out of {n_runs}", end="\n")
+    RMSE_filter.append(rmse_filter)
+    RMSE_smoother.append(rmse_smoother)
+
+
+# print(f"{np.mean(RMSE_filter) = }")
+# print(f"{np.mean(RMSE_smoother) = }")
+# print(f"{np.std(RMSE_filter) = }")
+# print(f"{np.std(RMSE_smoother) = }")
+# jnp.savez("results/rmse_experiment_L16_N200_100runs.npz", RMSE_filter_mean=np.mean(RMSE_filter), RMSE_smoother_mean=np.mean(RMSE_smoother))
